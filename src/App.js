3ceaff37bb8ca29f02c69f2682db345eaf53cc88
Luka Čopar <luka.copar@gmail.com>
2019-01-08 12:06:46 +0100

    added header and meme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
+import Main from './components/Main';
+import Header from './components/Header';
+
 class App extends Component {
   render() {
-    return <div>lmao xD svet!</div>;
+    return (
+      <React.Fragment>
+        <Main />
+      </React.Fragment>
+    )
   }
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
-export default class Main extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+import Meme from './Meme';
+import Header from './Header';
 
+export default class Main extends Component {
+    render() {
+        return (
+            <div className="container main-content">
+                <div className="grid">
+                    <React.Fragment>
+                        <Header />
+                        <Meme />
+                        <Meme />
+                        <Meme />
+                        <Meme />
+                    </React.Fragment>
+                </div>
+            </div>
+        )
+    }
+}
